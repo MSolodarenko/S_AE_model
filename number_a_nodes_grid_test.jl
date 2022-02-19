77e04cba23c679403a2ab29bba348639fb11f5aa
Maksym Solodarenko (PGR) <2288144S@student.gla.ac.uk>
2022-02-19 16:51:53 +0000

    Run AllubErosa and steady_state procedures for different sizes of grid on a_nodes

--- number_a_nodes_grid_test.jl
+++ number_a_nodes_grid_test.jl
@@ -11,7 +11,7 @@ include("Functions/steady_state.jl")
 # global parameters of the model's code
 #                   1           2           3       4
 #                gen_tol_x, gen_tol_f, distr_tol, val_tol
-GLOBAL_PARAMS = [1e-6, 1e-4, 1e-9, 1e-7]#[1e-8, 1e-4, 1e-9, 1e-7]#[1e-8, 1e-4, 1e-12, 1e-9]#[1e-8, 1e-4, 1e-7, 1e-5]#
+GLOBAL_PARAMS = [1e-6, 1e-4, 1e-9, 1e-10]#[1e-8, 1e-4, 1e-9, 1e-7]#[1e-8, 1e-4, 1e-12, 1e-9]#[1e-8, 1e-4, 1e-7, 1e-5]#
 
 # global parameters of the approximation objects
 #                               1               2               3               4                       5                   6
@@ -64,94 +64,141 @@ fig_output = false
 calc_add_results = false
 
 country = "Italy"
+#NUMBER_A_NODES = 49#15
 guess_R = -0.008174259548335908#(r_min+r_max)/2#
 guess_W = 0.23529682939411575#(w_min+w_max)/2#
 
 MODEL_PARAMS = [LAMBDA, BETA, DELTA, GAMMA, ETA, THETA, C_E, RHO_M, RHO_W, SIGMA_EPS_M, RHO_EPS_M_W, SIGMA_ZETA, P_ALPHA, ETA_ALPHA, PROB_NODE1_ALPHA, MU_M_ALPHA, RHO_ALPHA_M_W, SIGMA_ALPHA_W, SIGMA_EPS_W, CRRA]
-
-println_sameline(("NUMBER_A_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
-for NUMBER_A_NODES = 15:2:99#49
-    global_approx_params = [NUMBER_A_NODES,3,3,3,6,3]
-
-    start_time = time()
-    approx_object = build_skill_nodes(global_approx_params, MODEL_PARAMS)
-    res = AllubErosa(guess_R,guess_W, GLOBAL_PARAMS, global_approx_params, MODEL_PARAMS, approx_object)
-
-    time_length = round(time() - start_time; digits=0)
-    len_r = round(res[10]; digits=7)
-    len_w = round(res[11]; digits=7)
-    cap_out=round(res[12]; digits=7)
-    cre_out=round(res[13]; digits=7)
-    occ_w = round(res[14]; digits=7)
-    occ_se= round(res[15]; digits=7)
-    occ_emp=round(res[16]; digits=7)
-
-    println_sameline((NUMBER_A_NODES,time_length,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
-end
-throw(error)
-NUMBER_A_NODES = 15
-println_sameline(("NUMBER_U_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
-for NUMBER_U_NODES = 3:2:9#49
-    global_approx_params = [NUMBER_A_NODES,NUMBER_U_NODES,NUMBER_U_NODES,3,6,3]
-
-    start_time = time()
-    approx_object = build_skill_nodes(global_approx_params, MODEL_PARAMS)
-    res = AllubErosa(guess_R,guess_W, GLOBAL_PARAMS, global_approx_params, MODEL_PARAMS, approx_object)
-
-    time_length = round(time() - start_time; digits=0)
-    len_r = round(res[10]; digits=7)
-    len_w = round(res[11]; digits=7)
-    cap_out=round(res[12]; digits=7)
-    cre_out=round(res[13]; digits=7)
-    occ_w = round(res[14]; digits=7)
-    occ_se= round(res[15]; digits=7)
-    occ_emp=round(res[16]; digits=7)
-
-    println_sameline((NUMBER_A_NODES,time_length,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
-end
-
-
-
 #=
-# ss_star = [res, r, w, approx_object, MODEL_PARAMS]
-@time ss_star = steady_state(guess_R, guess_W, GLOBAL_PARAMS,GLOBAL_APPROX_PARAMS, MODEL_PARAMS)
-SS = copy(ss_star)
+open("Results/number_a_nodes_grid_results.txt","a") do io
+    guess_R = -0.008174259548335908#(r_min+r_max)/2#
+    guess_W = 0.23529682939411575#(w_min+w_max)/2#
+    println_sameline(("NUMBER_A_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    println(io,("NUMBER_A_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    for NUMBER_A_NODES = 15:2:53
+        try
+            global_approx_params = [NUMBER_A_NODES,3,3,3,6,3]
 
-LOCAL_DIR = "$(@__DIR__)/Results/"
-if Sys.iswindows()
-    LOCAL_DIR = "$(@__DIR__)\\Results\\"
+            start_time = time()
+            approx_object = build_skill_nodes(global_approx_params, MODEL_PARAMS)
+            res = AllubErosa(guess_R,guess_W, GLOBAL_PARAMS, global_approx_params, MODEL_PARAMS, approx_object)
+
+            time_length = round(time() - start_time; digits=0)
+            len_r = round(res[10]; digits=7)
+            len_w = round(res[11]; digits=7)
+            cap_out=round(res[12]; digits=7)
+            cre_out=round(res[13]; digits=7)
+            occ_w = round(res[14]; digits=7)
+            occ_se= round(res[15]; digits=7)
+            occ_emp=round(res[16]; digits=7)
+
+            println_sameline((NUMBER_A_NODES,time_length,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+            println(io,(NUMBER_A_NODES,time_length,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+        catch e
+            println_sameline((NUMBER_A_NODES,e))
+            println(io,(NUMBER_A_NODES,"failed to converge in time"))
+        end
+    end
 end
-
-open("$(LOCAL_DIR)res_$(round(lambda;digits=2)).txt", "a") do f
-    write(f, "Lambda: $(lambda)\n")
-
-    write(f, "Interest rate: $(ss_star[2])\n")
-    write(f, "Wage: $(ss_star[3])\n")
-    write(f, "\n")
-    write(f, "\nCapital-to-output: $(ss_star[1][12])\n")
-    write(f, "Credit-to-output: $(ss_star[1][13])\n")
-
-    write(f, "Share of workers: $(ss_star[1][14])\n")
-    write(f, "Share of self-employed: $(ss_star[1][15])\n")
-    write(f, "Share of employers: $(ss_star[1][16])\n")
-
-    write(f, "Variance of log-consumption: $(ss_star[1][17])\n")
-
-    write(f, "Occupational transition probabilities:\n")
-    write(f, "$(ss_star[1][18])\n")
-
-    write(f, "Variance of log-earnings: $(ss_star[1][19])\n")
-
-    write(f, "Gini for worker's income: $(ss_star[1][20])\n")
-    write(f, "Gini for entrepreneurial's income: $(ss_star[1][21])\n")
-
-    write(f, "\n------Productivity results---------\n")
-    write(f, "TFP_ideal: $(ss_star[1][43][end-4]*100)%\n")
-    write(f, "TFP_data: $(ss_star[1][43][end-3]*100)%\n")
-    write(f, "Labour productivity: $(ss_star[1][43][end-2])\n")
-    write(f, "Capital productivity (Output-to-capital): $(ss_star[1][43][end-1])\n")
-    write(f, "Capital productivity (Expenses-to-revenue = (r+delta)*Capital-earnings): $(ss_star[1][43][end])\n")
-end
-
-@save "$(LOCAL_DIR)SS_lambda_$(round(lambda;digits=2)).jld2" SS
 =#
+open("Results/number_a_nodes_ss_grid_results.txt","a") do io
+    guess_R = -0.008174259548335908#(r_min+r_max)/2#
+    guess_W = 0.23529682939411575#(w_min+w_max)/2#
+    println_sameline(("NUMBER_A_NODES","time_length","r","w","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    println(io,("NUMBER_A_NODES","time_length","r","w","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    for NUMBER_A_NODES = 15:2:53
+        try
+            global_approx_params = [NUMBER_A_NODES,3,3,3,6,3]
+
+            start_time = time()
+            ss_star = steady_state(guess_R, guess_W, GLOBAL_PARAMS,global_approx_params, MODEL_PARAMS)
+
+            time_length = round(time() - start_time; digits=0)
+            r     = round(ss_star[2];     digits=7)
+            w     = round(ss_star[3];     digits=7)
+            len_r = round(ss_star[1][10]; digits=7)
+            len_w = round(ss_star[1][11]; digits=7)
+            cap_out=round(ss_star[1][12]; digits=7)
+            cre_out=round(ss_star[1][13]; digits=7)
+            occ_w = round(ss_star[1][14]; digits=7)
+            occ_se= round(ss_star[1][15]; digits=7)
+            occ_emp=round(ss_star[1][16]; digits=7)
+
+            println_sameline((NUMBER_A_NODES,time_length,r,w,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+            println(io,      (NUMBER_A_NODES,time_length,r,w,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+
+            guess_R = r
+            guess_W = w
+        catch e
+            println_sameline((NUMBER_A_NODES,e))
+            println(io,(NUMBER_A_NODES,"failed to converge in time"))
+        end
+    end
+end
+
+open("Results/number_u_nodes_grid_results.txt","a") do io
+    guess_R = -0.008174259548335908#(r_min+r_max)/2#
+    guess_W = 0.23529682939411575#(w_min+w_max)/2#
+    NUMBER_A_NODES = 49#15
+    println_sameline(("NUMBER_U_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    println(io,("NUMBER_U_NODES","time_length","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    for NUMBER_U_NODES = 3:2:9
+        try
+            global_approx_params = [NUMBER_A_NODES,NUMBER_U_NODES,NUMBER_U_NODES,3,6,3]
+
+            start_time = time()
+            approx_object = build_skill_nodes(global_approx_params, MODEL_PARAMS)
+            res = AllubErosa(guess_R,guess_W, GLOBAL_PARAMS, global_approx_params, MODEL_PARAMS, approx_object)
+
+            time_length = round(time() - start_time; digits=0)
+            len_r = round(res[10]; digits=7)
+            len_w = round(res[11]; digits=7)
+            cap_out=round(res[12]; digits=7)
+            cre_out=round(res[13]; digits=7)
+            occ_w = round(res[14]; digits=7)
+            occ_se= round(res[15]; digits=7)
+            occ_emp=round(res[16]; digits=7)
+
+            println_sameline((NUMBER_A_NODES,time_length,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+        catch e
+            println_sameline((NUMBER_U_NODES,e))
+            println(io,(NUMBER_U_NODES,"failed to converge in time"))
+        end
+    end
+end
+
+open("Results/number_u_nodes_ss_grid_results.txt","a") do io
+    guess_R = -0.008174259548335908#(r_min+r_max)/2#
+    guess_W = 0.23529682939411575#(w_min+w_max)/2#
+    NUMBER_A_NODES = 49#15
+    println_sameline(("NUMBER_U_NODES","time_length","r","w","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    println(io,("NUMBER_U_NODES","time_length","r","w","len_r","len_w","cap_out","cre_out","occ_w","occ_se","occ_emp"))
+    for NUMBER_U_NODES = 3:2:9
+        try
+            global_approx_params = [NUMBER_A_NODES,NUMBER_U_NODES,NUMBER_U_NODES,3,6,3]
+
+            start_time = time()
+            ss_star = steady_state(guess_R, guess_W, GLOBAL_PARAMS,global_approx_params, MODEL_PARAMS)
+
+            time_length = round(time() - start_time; digits=0)
+            r     = round(ss_star[2];     digits=7)
+            w     = round(ss_star[3];     digits=7)
+            len_r = round(ss_star[1][10]; digits=7)
+            len_w = round(ss_star[1][11]; digits=7)
+            cap_out=round(ss_star[1][12]; digits=7)
+            cre_out=round(ss_star[1][13]; digits=7)
+            occ_w = round(ss_star[1][14]; digits=7)
+            occ_se= round(ss_star[1][15]; digits=7)
+            occ_emp=round(ss_star[1][16]; digits=7)
+
+            println_sameline((NUMBER_U_NODES,time_length,r,w,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+            println(io,      (NUMBER_U_NODES,time_length,r,w,len_r,len_w,cap_out,cre_out,occ_w,occ_se,occ_emp))
+
+            guess_R = r
+            guess_W = w
+        catch e
+            println_sameline((NUMBER_U_NODES,e))
+            println(io,(NUMBER_U_NODES,"failed to converge in time"))
+        end
+    end
+end
