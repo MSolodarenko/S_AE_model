d180f22ae17735815abc3a1025cf0ef7911c9fb1
Maksym Solodarenko <msolodarenko@gmail.com>
2022-04-22 15:09:35 +0100

    Add empty print_sameline()

--- Functions/print_sameline.jl
+++ Functions/print_sameline.jl
@@ -1,4 +1,9 @@
 
+function println_sameline()
+    print("\e[2K") # clear whole line
+    print("\e[1G") # move cursor to column 1
+    println()
+end
 
 function println_sameline(text)
     print("\e[2K") # clear whole line
@@ -6,6 +11,12 @@ function println_sameline(text)
     println(text)
 end
 
+
+function print_sameline()
+    print("\e[2K") # clear whole line
+    print("\e[1G") # move cursor to column 1
+    print()
+end
 
 function print_sameline(text)
     print("\e[2K") # clear whole line
