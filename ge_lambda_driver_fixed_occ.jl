fe95139d36f32dbe1ec5d9a5406aa43b09e5af66
Maksym Solodarenko <msolodarenko@gmail.com>
2022-08-12 12:54:21 +0100

    Update parameters in fixed_occ

--- ge_lambda_driver_fixed_occ.jl
+++ ge_lambda_driver_fixed_occ.jl
@@ -16,34 +16,37 @@ GLOBAL_PARAMS = [1e-6, 1e-4, 1e-9, 1e-7]#[1e-8, 1e-4, 1e-9, 1e-7]#[1e-8, 1e-4, 1
 # global parameters of the approximation objects
 #                               1               2               3               4                       5                   6
 #                       number_a_nodes, number_u_m_nodes, number_u_w_nodes, number_zeta_nodes, number_alpha_m_nodes, number_alpha_w_nodes
-GLOBAL_APPROX_PARAMS = [15,3,3,3,6,3]#[49,3,3,3,6,3]#[25,5,5,3,6,3]#
+GLOBAL_APPROX_PARAMS = [69,3,3,3,6,3]#[35,3,3,3,6,3]
 
+CODENAME = #="SS_2642"#"SS_2065"=#"SS_2092"
+CODENAME = "$(CODENAME)_$(GLOBAL_APPROX_PARAMS[1])"
 # parameters of the model's economy (Italy)
-LAMBDA = 1.513028#1.548387
-BETA = 0.917506#0.859906
-DELTA = 0.1
-GAMMA = 0.16
-ETA = 0.3
-THETA = 0.54
+                    #SS_2642 #SS_2065 #SS_2092 #prev.calibration
+LAMBDA =            #=1.633951#1.405096=#1.665907 #1.513028
+BETA =              #=0.923514#0.910782=#0.934172 #0.917506
+DELTA =             0.1
+GAMMA =             0.16
+ETA =               0.3
+THETA =             0.54
 
-C_E = 0.007001#0.036321
-RHO_M = 0.710266#0.849951
-RHO_W = 0.96
-SIGMA_EPS_M = 1.022857#0.942809
-RHO_EPS_M_W = 0.361605#0.384691
-SIGMA_ZETA = 0.091089#0.151709
-P_ALPHA = 0.024445#0.043692
-ETA_ALPHA = 5.896035#5.202754
-PROB_NODE1_ALPHA = 0.39
-MU_M_ALPHA = -4.817675#-3.909137
-RHO_ALPHA_M_W = 0.181454#0.095903
-SIGMA_ALPHA_W = 0.12
-SIGMA_EPS_W = 0.021947#0.285519
+C_E =               #=0.034618#0.011300=#0.057572 #0.007001
+RHO_M =             #=0.961272#0.963735=#0.951032 #0.710266
+RHO_W =             0.96
+SIGMA_EPS_M =       #=0.996198#0.877067=#0.754644 #1.022857
+RHO_EPS_M_W =       #=0.296954#0.309361=#0.216037 #0.361605
+SIGMA_ZETA =        #=0.256800#0.247935=#0.211565 #0.091089
+P_ALPHA =           #=0.039732#0.034184=#0.022080 #0.024445
+ETA_ALPHA =         #=5.860434#5.567285=#5.811008 #5.896035
+PROB_NODE1_ALPHA =  0.39
+MU_M_ALPHA =       #=-4.950012#-2.089921=#-3.266806#-4.817675
+RHO_ALPHA_M_W =     #=0.103225#0.166184=#0.147661 #0.181454
+SIGMA_ALPHA_W =     0.12
+SIGMA_EPS_W =       #=0.119669#0.152209=#0.048689 #0.021947
 
 CRRA = 1.0 # >0.0
 
-R_ = -0.005338#-0.069128
-W_ = 0.230294#0.393906
+R_ =                #=-2.627421/100#-5.147463/100=#1.434096/100#-0.005338
+W_ =                #=0.136814#0.287390=#0.294537 #0.230294
 
 gen_tol_x = GLOBAL_PARAMS[1]
 gen_tol_f = GLOBAL_PARAMS[2]
@@ -64,30 +67,28 @@ fig_output = false
 calc_add_results = false
 
 include("Functions/AllubErosa_fixed_occ.jl")
-OCC_SHARES = [0.6724, 0.1025, 0.2251]
+OCC_SHARES = [0.77445, 0.06473, 0.16082]
 function AllubErosa(r,w,global_params,global_approx_params,model_params,approx_object)
     return AllubErosa(r,w,global_params,global_approx_params,model_params,approx_object,OCC_SHARES)
 end
 
 # Generate gen eq'm for different lambdas
-lambdas = [collect(range(1.01; length = 5, stop = 1.270437995455));
-           collect(range(1.27; length = 6, stop = 1.513028)      )[2:end];
-           collect(range(1.51; length = 4, stop = 2.0)           )[2:end];
+lambdas = #=[1.513028, 2.6]=#[collect(range(1.01; length = 5, stop = 1.270437995455));
+           collect(range(1.27; length = 6, stop = LAMBDA)      )[2:end];
+           collect(range(LAMBDA; length = 4, stop = 2.0)           )[2:end];
            collect(range(2.0;  length = 6, stop = 5.0)           )[2:end];
-           collect(range(5.0;  length = 6, stop = 10.0)          )[2:end];
+           collect(range(5.0;  length = 6, stop = 10.0)          )[2:end]#=;
            collect(range(10.0; length = 5, stop = 25.0)          )[2:end];
            collect(range(25.0; length = 4, stop = 100.0)         )[2:end];
-           collect(range(100.0;length = 4, stop = 2500.0)        )[2:end]]
+           collect(range(100.0;length = 4, stop = 2500.0)        )[2:end]=#]
 
-l1_i = findfirst(x -> x==1.513028, lambdas)
-l2_i = findfirst(x -> (1.67-1e-2 < x)&&(x > 1.67+1e-2), lambdas)
-l3_i = findfirst(x -> (2.6-1e-2 < x)&&(x > 2.6+1e-2), lambdas)
+l1_i = findfirst(x -> x==LAMBDA, lambdas)
 
-LOCAL_DIR_BRAZIL = "$(@__DIR__)/Results/Fixed_occ_shares/Lambda_grid/Brazil/"
-LOCAL_DIR_ITALY = "$(@__DIR__)/Results/Fixed_occ_shares/Lambda_grid/Italy/"
+LOCAL_DIR_BRAZIL = "$(@__DIR__)/Results/Fixed_occ_shares/Lambda_grid/Brazil_$(CODENAME)/"
+LOCAL_DIR_ITALY = "$(@__DIR__)/Results/Fixed_occ_shares/Lambda_grid/Italy_$(CODENAME)/"
 if Sys.iswindows()
-   LOCAL_DIR_BRAZIL = "$(@__DIR__)\\Results\\Fixed_occ_shares\\Lambda_grid\\Brazil\\"
-   LOCAL_DIR_ITALY = "$(@__DIR__)\\Results\\Fixed_occ_shares\\Lambda_grid\\Italy\\"
+   LOCAL_DIR_BRAZIL = "$(@__DIR__)\\Results\\Fixed_occ_shares\\Lambda_grid\\Brazil_$(CODENAME)\\"
+   LOCAL_DIR_ITALY = "$(@__DIR__)\\Results\\Fixed_occ_shares\\Lambda_grid\\Italy_$(CODENAME)\\"
 end
 for country = ["Italy"#=, "Brazil"=#]
     if country == "Italy"
@@ -99,8 +100,8 @@ for country = ["Italy"#=, "Brazil"=#]
     end
     mkpath(LOCAL_DIR)
 
-    guess_R = -6.323809959525245/100#-0.4933/100#(r_min+r_max)/2#
-    guess_W = 0.19983052719342714#0.22939#(w_min+w_max)/2#
+    guess_R = R_#-9.88337/100#(r_min+r_max)/2#
+    guess_W = W_#0.2357213#(w_min+w_max)/2#
 
     Rs = zeros(length(lambdas))
     Ws = zeros(length(lambdas))
@@ -123,8 +124,12 @@ for country = ["Italy"#=, "Brazil"=#]
 
     SSS = Array{Any}(undef,length(lambdas))
 
-    for i = 1:length(lambdas)
-        println("\n$(country) - $(i)/$(length(lambdas))")
+    for i = [l1_i; l1_i-1:-1:1; l1_i+1:1:length(lambdas)]#1:length(lambdas)#[[l1_i, l3_i, l2_i]; deleteat!(collect(1:length(lambdas)),[l1_i, l2_i, l3_i])]#
+        println("\n$(country) - $(i)/$(length(lambdas)) - $(lambdas[i])")
+        if i >= l1_i-1 && i <= l1_i+1
+            guess_R = R_
+            guess_W = W_
+        end
         MODEL_PARAMS = zeros(19)
         if country == "Brazil"
             #           1       2     3     4       5   6       7   8       9       10          11              12      13          14          15              16          17              18              19       20
@@ -144,8 +149,21 @@ for country = ["Italy"#=, "Brazil"=#]
             println("Already calculated")
             ss_star = copy(SS)
         catch e
-            # ss_star = [res, r, w, approx_object, model_params]
-            @time ss_star = steady_state(guess_R, guess_W, GLOBAL_PARAMS,GLOBAL_APPROX_PARAMS, MODEL_PARAMS)
+            try
+                # ss_star = [res, r, w, approx_object, model_params]
+                @time ss_star = steady_state(guess_R, guess_W, GLOBAL_PARAMS,GLOBAL_APPROX_PARAMS, MODEL_PARAMS)
+            catch e
+                println_sameline("Calculation has failed")
+                try
+                    try
+                        ss_star = copy(SSS[i-1])
+                    catch eee
+                        ss_star = copy(SSS[i+1])
+                    end
+                catch ee
+                    nothing
+                end
+            end
             SSS[i] = copy(ss_star)
 
             SS = copy(ss_star)
